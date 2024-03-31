f200bd072a6f33c6987c6ae00273ff5160674a1e
school17729 <school17729@gmail.com>
2024-03-31 00:42:32 -0400

    Removed js/ and node_modules/ directories

--- js/SaveBlock.js
+++
@@ -1,6 +0,0 @@
-var SaveBlock;
-(function (SaveBlock) {
-    SaveBlock[SaveBlock["A"] = 0] = "A";
-    SaveBlock[SaveBlock["B"] = 1] = "B";
-})(SaveBlock || (SaveBlock = {}));
-export { SaveBlock };
